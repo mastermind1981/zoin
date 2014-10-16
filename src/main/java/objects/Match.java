9d28f58b14fdbf61cbca40ba4dd155cd9b764ac5
someone@zoin.net <someone@zoin.net>
2014-10-16 16:36:11 +0200

    Added match class

---
+++ src/main/java/objects/Match.java
@@ -0,0 +1,22 @@
+package objects;
+
+public class Match {
+	
+	private int value;
+	private int missionID;
+	private int heroId;
+	
+	public Match() {
+		
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	public int getMissionID() {
+		return missionID;
+	}
+	public int getHeroId() {
+		return heroId;
+	}
+}
