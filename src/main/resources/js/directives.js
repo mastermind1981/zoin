f6140b5ae93acfb56338427c73e2c25993885030
r3dDoX <little_mc_walther@hotmail.com>
2014-10-16 15:40:13 -0700

    added header directive

--- src/main/resources/js/directives.js
+++ src/main/resources/js/directives.js
@@ -10,4 +10,20 @@ angular.module('myApp.directives', [])
         return function (scope, elm, attrs) {
             elm.text(version);
         };
-  }]);
+    }]
+).directive('header', function (zoinAPIService) {
+    return {
+        templateUrl: 'partials/header.html',
+        replace: true,
+        link: function ($scope, element, attrs) {
+            $scope.init = function() {
+                $scope.hero = zoinAPIService.Hero.get({"heroId": $scope.heroId});
+            }
+
+            $scope.init();
+            $scope.$watch('heroId', function() {
+                $scope.init();
+            });
+        }
+    }
+ });
