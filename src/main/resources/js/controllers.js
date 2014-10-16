bfb1de1072264c6881906cb9d8481e8e065b9c94
bmillo <bmillo@gmail.com>
2014-10-16 11:10:26 -0700

    added global hero id

--- src/main/resources/js/controllers.js
+++ src/main/resources/js/controllers.js
@@ -3,19 +3,38 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-
-.controller('HeroCardCtrl', function ($scope, zoinAPIService) {
-    $scope.hero = zoinAPIService.Hero.get({"heroId": "931"}); // Example to filter hero according ID    
+.run(function($rootScope) {
+    $rootScope.heroId = "931";
 })
 
-.controller('MissionCardCtrl', function ($scope, zoinAPIService) {
+.controller('HeroCardCtrl', function ($scope, $rootScope, zoinAPIService) { 
+    $scope.init = function() {
+        $scope.hero = zoinAPIService.Hero.get({"heroId": $rootScope.heroId}); // Example to filter hero according ID  
+    }
+    
+    $scope.init();    
+     $scope.$watch('heroId', function() {
+       $scope.init();
+   });
+})
+
+.controller('MissionCardCtrl', function ($scope, $rootScope, zoinAPIService) {
     $scope.mission = zoinAPIService.Mission.get({"missionId": "1"});
 })
 
-.controller('DashboardCtrl', function ($scope, zoinAPIService) {
-    $scope.missions = zoinAPIService.Mission.query();
-    $scope.match = zoinAPIService.Match.get({"heroId": "931"});
-    $scope.match = zoinAPIService.Match.query({"heroId": "931"});
+.controller('DashboardCtrl', function ($scope, $rootScope, zoinAPIService) {
+    $scope.init = function() {
+        $scope.missions = zoinAPIService.Mission.query();
+        $scope.match = zoinAPIService.Match.query({"heroId": $rootScope.heroId});
+    }
+    
+    $scope.init();    
+     $scope.$watch('heroId', function() {
+       $scope.init();
+   });
+    
+    
+    
 })
 
 .controller('ApiCtrl', function ($scope) {
