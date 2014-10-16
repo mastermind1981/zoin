4bb59bb9ba9c5a773ab5fc34717a6cec4dd3799d
someone@zoin.net <someone@zoin.net>
2014-10-16 06:02:21 -0700

    set hero id to frank

--- src/main/resources/js/controllers.js
+++ src/main/resources/js/controllers.js
@@ -3,5 +3,5 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).controller('MyCtrl1', function ($scope, zoinAPIService) {
-    $scope.hero = zoinAPIService.Hero.get({"heroId": "1234"}); // Example to filter hero according ID
+    $scope.hero = zoinAPIService.Hero.get({"heroId": "931"}); // Example to filter hero according ID
 });
