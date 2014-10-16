c50849eca44b4ff1d9cac93cfc0fde9d9009734e
someone@zoin.net <someone@zoin.net>
2014-10-16 05:57:56 -0700

    fixed rest call to url part for hero id

--- src/main/resources/js/services.js
+++ src/main/resources/js/services.js
@@ -9,7 +9,7 @@ angular.module('myApp.services', []).factory('zoinAPIService', function ($resour
 
     var zoinAPI = {};
 
-    zoinAPI.Hero = $resource('/zoin/rest-prefix/heroes/:heroId', {heroId:'@id'});
+    zoinAPI.Hero = $resource('/zoin/rest-prefix/heroes/:heroId/', {heroId:'@id'});
 
     return zoinAPI;
 });
