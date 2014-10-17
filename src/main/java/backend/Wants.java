c2bb2cda6445e44d83fa43db484abcacdbc4c157
Frank Beeh <fbe@zuehlke.com>
2014-10-17 01:03:54 -0700

    Decrease zoin amount when wanting a project.

--- src/main/java/backend/Wants.java
+++ src/main/java/backend/Wants.java
@@ -29,20 +29,24 @@ public class Wants {
 
 	@PersistenceContext
 	EntityManager em;
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces("application/json;charset=" + encoding)
 	@Path("")
-	public void postMission(WantRequest request) throws JsonGenerationException, JsonMappingException, IOException {
-		TypedQuery<Hero> q1 = em.createQuery("SELECT x FROM Hero x WHERE id='"+request.getHeroId()+"'",
-				Hero.class);
+	public void postMission(WantRequest request)
+			throws JsonGenerationException, JsonMappingException, IOException {
+		TypedQuery<Hero> q1 = em.createQuery("SELECT x FROM Hero x WHERE id='"
+				+ request.getHeroId() + "'", Hero.class);
 		Hero hero = q1.getSingleResult();
+		hero.removeZoins(request.getZoins());
+		em.persist(hero);
 		
-		TypedQuery<Mission> q2 = em.createQuery("SELECT x FROM Mission x WHERE id='"+request.getMissionId()+"'",
-				Mission.class);
+		TypedQuery<Mission> q2 = em.createQuery(
+				"SELECT x FROM Mission x WHERE id='" + request.getMissionId()
+						+ "'", Mission.class);
 		Mission mission = q2.getSingleResult();
-		
+
 		Want want = new Want(hero, mission, request.getZoins());
 		em.persist(want);
 	}
