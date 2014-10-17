560eba4e83241e3b9c399cf19ab2135c8e534d22
Frank Beeh <fbe@zuehlke.com>
2014-10-17 00:34:02 -0700

    Compute correct score for education target.

--- src/main/java/scoring/Scoring.java
+++ src/main/java/scoring/Scoring.java
@@ -8,45 +8,73 @@ import jpa.Hero;
 import jpa.Mission;
 import jpa.Skill;
 import objects.Score;
+import objects.SkillMatch;
 
 public class Scoring {
 	private static final int SCORE_FOR_MATCHING_SKILL = 1;
-	private static final int SCORE_FOR_MATCHING_EDUCATION_TARGET = 2;
+	private static final int ADDITIONAL_SCORE_FOR_MATCHING_EDUCATION_TARGET = 1;
 	private static final int SCORE_FOR_MATCHING_ROLE = 10;
 
 	public Score computeScoreForHero(Hero hero, Mission mission, Integer zoins) {
-		return computeScore(mission.getSkillSet().getSkills(), hero.getSkillSet().getSkills(),
-				hero.getEducationTarget(), hero.getRole().equals(mission.getRole()), zoins);
+		return computeScore(mission.getSkillSet().getSkills(), hero
+				.getSkillSet().getSkills(), hero.getEducationTarget(), hero
+				.getRole().equals(mission.getRole()), zoins);
 	}
 
 	public Score computeScoreForMission(Mission mission, Hero hero,
 			Integer zoins) {
-		return computeScore(hero.getSkillSet().getSkills(), mission.getSkillSet().getSkills(),
-				hero.getEducationTarget(), hero.getRole().equals(mission.getRole()), zoins);
+		return computeScore(hero.getSkillSet().getSkills(), mission
+				.getSkillSet().getSkills(), hero.getEducationTarget(), hero
+				.getRole().equals(mission.getRole()), zoins);
 	}
 
 	private Score computeScore(final Set<Skill> requestedSkills,
 			final Set<Skill> availableSkills, Skill educationTarget,
 			final boolean roleMatching, Integer zoins) {
-		int compureScoreBySkills = compureScoreBySkills(requestedSkills,
+		int scoreBySkills = compureScoreBySkills(requestedSkills,
 				availableSkills, educationTarget);
-		return new Score(computeTotalScore(roleMatching, compureScoreBySkills,
-				zoins), roleMatching, computeSkillMatches(requestedSkills,
-				availableSkills), compureScoreBySkills);
+		return new Score(computeTotalScore(roleMatching, scoreBySkills,
+				requestedSkills, educationTarget, zoins), roleMatching,
+				computeSkillMatches(requestedSkills, availableSkills,
+						educationTarget), scoreBySkills);
 	}
 
 	private int computeTotalScore(final boolean roleMatching,
-			int compureScoreBySkills, Integer zoins) {
+			int compureScoreBySkills, Set<Skill> requestedSkills,
+			Skill educationTarget, Integer zoins) {
 		return (roleMatching ? SCORE_FOR_MATCHING_ROLE : 0)
-				+ compureScoreBySkills + (zoins == null ? 0 : zoins.intValue());
+				+ compureScoreBySkills
+				+ (zoins == null ? 0 : zoins.intValue()
+						+ computeScoreForEductionTarget(educationTarget,
+								requestedSkills));
 	}
 
-	private Map<Skill, Boolean> computeSkillMatches(Set<Skill> requestedSkills,
-			Set<Skill> availableSkills) {
-		final Map<Skill, Boolean> skillMatches = new TreeMap<Skill, Boolean>();
+	private int computeScoreForEductionTarget(Skill educationTarget,
+			Set<Skill> requestedSkills) {
+		if (educationTarget != null
+				&& requestedSkills.contains(educationTarget)) {
+			return ADDITIONAL_SCORE_FOR_MATCHING_EDUCATION_TARGET;
+		} else {
+			return 0;
+		}
+	}
+
+	private Map<Skill, SkillMatch> computeSkillMatches(
+			Set<Skill> requestedSkills, Set<Skill> availableSkills,
+			Skill educationTarget) {
+		final Map<Skill, SkillMatch> skillMatches = new TreeMap<Skill, SkillMatch>();
 		for (Skill requestedSkill : requestedSkills) {
-			skillMatches.put(requestedSkill,
-					availableSkills.contains(requestedSkill));
+			SkillMatch skillMatch;
+			if (requestedSkill.equals(educationTarget)) {
+				skillMatch = SkillMatch.EducationTarget;
+			} else {
+				if (availableSkills.contains(requestedSkill)) {
+					skillMatch = SkillMatch.Yes;
+				} else {
+					skillMatch = SkillMatch.No;
+				}
+			}
+			skillMatches.put(requestedSkill, skillMatch);
 		}
 		return skillMatches;
 	}
@@ -58,9 +86,9 @@ public class Scoring {
 			if (availableSkills.contains(requestedSkill)) {
 				score += SCORE_FOR_MATCHING_SKILL;
 			}
-			if (requestedSkill.equals(educationTarget)) {
-				score += SCORE_FOR_MATCHING_EDUCATION_TARGET;
-			}
+		}
+		if (requestedSkills.contains(educationTarget)) {
+			score += SCORE_FOR_MATCHING_SKILL;
 		}
 		return score;
 	}
